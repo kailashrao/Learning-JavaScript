09681e3d41b0355f0da09f44b815688c16b5b3ba
kailashrao <kailash7rao@gmail.com>
2023-10-13 11:46:27 -0700

    Display email in console

--- Mailing List/index.js
+++ Mailing List/index.js
@@ -1,4 +1,4 @@
-const file = require('fs'); // Requires Node.js module
+// const file = require('fs'); // Node.js module (not vanilla JS)
 
 let emailInput = document.getElementById("emailForm");
 
@@ -7,16 +7,24 @@ function storeEmail(eventObj) {
 
     let email = document.getElementById("email-input");
 
-    console.log(email.value);
+    
 
+    
     if(email.value == "") {
-        document.getElementById("error").innerHTML = "Please enter valid email.";
+        document.getElementById("msg").innerHTML = "Please enter valid email!";
     } else {
+        console.log(email.value);
+        document.getElementById("msg").innerHTML = "Success!";
+
+        /* Requires node.js "fs" (file system) module 
         file.writeFile('emails.txt', email.value, (error) => {
             if(error)
                 console.log("Error");
         });
+        */
+
     }
+    
 }
 
 emailInput.addEventListener("submit", storeEmail);
