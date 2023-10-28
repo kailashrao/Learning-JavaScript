864549bcb6a18dc47ddf00ad8bb989b0c6f18a31
kailashrao <kailash7rao@gmail.com>
2023-10-28 10:14:39 -0700

    Calculate Bank 2 Interest

--- Bank Interest/interest.js
+++ Bank Interest/interest.js
@@ -23,67 +23,74 @@ Then display both interest amounts and tell customer which Bank is offering a be
 
 */
 
-function interestBank1(rowData, allDays) {
-    let numRows = rowData.length;
-    let numDays = allDays.length;
-
-    let principle = 0;
+function interestBank1(dailyPrinciples, numDays) {
+ 
     let dailyInterests = [];
-
     let interest = 0;
+
     for(let i = 0; i < numDays; i++) {
-        let included = false;
-        for(let j = 1; j < numRows; j++) {
-            if(rowData[j][0].includes(allDays[i])) {
-
-                let credit = parseFloat(rowData[j][1]);
-                if (isNaN(credit)) {
-                    credit = 0;
-                }
-
-                let debit = parseFloat(rowData[j][2]);
-                if (isNaN(debit)) {
-                    debit = 0;
-                }
-
-                principle = principle + credit - debit;
-
-                interest = principle * (1/365) * (0.05);
-                dailyInterests.push(interest);
-
-                included = true;
-                break;
-            }
-        }
-
-        if(!included) {
-            dailyInterests.push(dailyInterests[i-1]);
-        }
+        interest = dailyPrinciples[i] * (1/365) * (0.05);
+        dailyInterests.push(interest);
     }
 
-    console.table(dailyInterests);
-
     let totalInterest = 0;
-    for(let i = 0; i < numDays - 1; i++) {
+    for(let i = 0; i < numDays; i++) {
         totalInterest += dailyInterests[i];
     }
 
-    console.log(totalInterest);
+    console.log("Bank 1 Interest: " + totalInterest);
+
+}
+
+function interestBank2(dailyPrinciples, numDays) {
+    // Find minimum principle for each month
+    let numMonths = numDays / 31;
+    let monthStart, monthEnd;
+
+    let minPrinciple = dailyPrinciples[0];
+
+    let interest;
+    let monthlyInterests = [];
+
+    for(let i = 0; i < numMonths; i++) {
+        monthStart = 31 * i;
+        monthEnd = 31 * (i + 1);
+
+        for(let j = monthStart; j < monthEnd; j++) {
+            
+            if (dailyPrinciples[j] < minPrinciple) {
+                minPrinciple = dailyPrinciples[j];
+            } 
+        }
+
+        interest = minPrinciple * (31/365) * (0.08);
+        monthlyInterests.push(interest);
+    }
+
+    // Sum monthly interest
+
+    let totalInterest = 0;
+    for(let i = 0; i < numMonths; i++) {
+        totalInterest += monthlyInterests[i];
+    }
+
+    console.log("Bank 2 Interest: " + totalInterest);
+
 
 }
 
 
 function formData(csvData){
     let lines = csvData.split("\n");
-    let numOfLines = lines.length;
+    let numRows = lines.length;
     let lineData = [];
 
-    for(let i = 0; i < numOfLines; i++) {
+    for(let i = 0; i < numRows; i++) {
         lineData.push(lines[i].split(","));
     }
 
     let startDate = new Date(lineData[1][0]);
-    let endDate = new Date(lineData[numOfLines-1][0]);
+    let endDate = new Date(lineData[numRows-1][0]);
 
     let allDaysArray = [];
 
@@ -95,9 +102,42 @@ function formData(csvData){
         allDaysArray.push((dayOfMonth + "-" + i.toLocaleString("en-US", {month: "short"}) + "-" + i.getFullYear()));
     }
 
-    console.table(allDaysArray);
+    let numDays = allDaysArray.length;
+    let dailyPrinciples = []
+    let principle = 0;
 
-    interestBank1(lineData, allDaysArray);
+    for(let i = 0; i < numDays; i++) {
+        let included = false;
+        for(let j = 1; j < numRows; j++) {
+            if(lineData[j][0].includes(allDaysArray[i])) {
+
+                let credit = parseFloat(lineData[j][1]);
+                if (isNaN(credit)) {
+                    credit = 0;
+                }
+
+                let debit = parseFloat(lineData[j][2]);
+                if (isNaN(debit)) {
+                    debit = 0;
+                }
+
+                principle = principle + credit - debit;
+
+                dailyPrinciples.push(principle);
+
+                included = true;
+                break;
+            }
+        }
+
+        if(!included) {
+            dailyPrinciples.push(dailyPrinciples[i-1]);
+        }
+    }
+
+
+    interestBank1(dailyPrinciples, numDays);
+    interestBank2(dailyPrinciples, numDays);
 
 }
 
