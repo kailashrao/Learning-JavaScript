0b9c326979c4fefcba8dfa44fd214aa8f6e285a4
kailashrao <kailash7rao@gmail.com>
2023-10-28 17:55:08 -0700

    Add new/renamed files

---
+++ Bank Interest/testPadStart.js
@@ -0,0 +1,8 @@
+let array1 = [2, 4, 6, 8, 10, 12];
+
+let str1 = array1[0].toString().padStart(4, "$");
+console.log(str1);
+
+// for(let i = 0; i < array1.length; i++) {
+//     console.log(array1[i].toString)
+// }
---
+++ Bank Interest/testTernary.js
@@ -0,0 +1,5 @@
+let even = [2, 4, 6, 8 ,10 ,12];
+
+for(let i = 0; i < even.length; i++) {
+    even[i] < 10 ? console.log(even[i]) : console.log("");
+}
