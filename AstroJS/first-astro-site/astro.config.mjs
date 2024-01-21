fd3b3924a92a1bd498f9a190c8bdf29728b6f036
kailashrao <kailash7rao@gmail.com>
2024-01-21 12:09:26 -0800

    save changes

--- AstroJS/first-astro-site/astro.config.mjs
+++ AstroJS/first-astro-site/astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://kailashrao.github.io',
-    base: '/Learning-JavaScript/AstroJS/first-astro-site',
+  site: 'https://kailashrao.github.io',
+  base: '/Learning-JavaScript/AstroJS/first-astro-site',
+  integrations: [tailwind()]
 });
