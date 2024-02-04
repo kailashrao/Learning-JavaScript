14da3c152f6c19ee8c5256447c82941793b3b965
kailashrao <kailash7rao@gmail.com>
2024-02-03 17:38:26 -0800

    Cleanup experimental features

--- World Map Timezones/edit.js
+++
@@ -1,9 +0,0 @@
-let menu = document.getElementById("menu");
-menu.addEventListener("click", showOptions);
-
-function showOptions(eventObj) {
-    eventObj.preventDefault();
-    document.getElementById("edit").style.display = "flex";
-    document.getElementById("menu").style.display = "none";
-    
-}
