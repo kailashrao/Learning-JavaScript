42938169c42f6e0d011c37c5d13a5e16cd8e5120
kailashrao <kailash7rao@gmail.com>
2024-03-09 18:07:31 -0800

    Seating Table + Custom Seating Dimensions

--- Checkboxes/app.js
+++ Checkboxes/app.js
@@ -1,21 +1,32 @@
 container = document.getElementById("seat-container");
 
 function generateRows(numRows, numSeats) {
-    let row1 = "";
-    for(let j = 1; j<(numRows+1); j++ ) {
+    let row1 = "<tr><td></td>";
+    for(let z = 1; z<(numSeats+1); z++){
+        row1 += "<th>" + z + "</th>";
+    }
+    for(let j = 1; j<(numRows+1); j++) {
 
-        row1 += "<div id='row" + j + "'>";
-        row1 += "<label value='" + j + "'>" + j + "</label>";
+        row1 += "<tr>";
+        row1 += "<th>" + j + "</th>";
         for(let i = 1; i<(numSeats+1) ; i++) {
-            row1+="<input id='" + i + "' type='checkbox'>";
+            row1+="<td><input id='" + j + '.' +  i + "' type='checkbox'></td>";
         }
-        row1 += "<label value='" + j + "'>" + j + "</label>";
-        row1 += "</div>";
+        row1 += "<th>" + j + "</th>";
+        row1 += "</tr>";
 
     }
     container.innerHTML = row1;
 }
 
-generateRows(10, 20);
 
 
+function customDimensions(event) {
+    event.preventDefault();
+    let numRows = Number(document.getElementById("numRows").value);
+    let numSeats = Number(document.getElementById("numSeats").value);
+
+    generateRows(numRows, numSeats);
+
+}
+document.getElementById("customForm").addEventListener("submit", customDimensions);
