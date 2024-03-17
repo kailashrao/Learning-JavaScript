564bbc9519b7786d60eb7e8048be781a13e83945
kailashrao <kailash7rao@gmail.com>
2024-03-16 18:02:10 -0700

    Use Row Letters

--- Checkboxes/app.js
+++ Checkboxes/app.js
@@ -1,18 +1,38 @@
 container = document.getElementById("seat-container");
 
+
+let alphabet1 = [];
+for(let i = 65; i<(65+26); i++){
+    alphabet1.push(String.fromCharCode(i));
+}
+
+function generateRowLetter(count1) {
+    for(let j = 0; j < (Math.ceil(count1/26) - 1); j++) {
+        for(let i = 0; i<26; i++){
+            alphabet1.push(alphabet1[j] + alphabet1[i]);
+        }
+    }
+}
+
+
+
+
 function generateRows(numRows, numSeats) {
+    numRows = Math.min(numRows, (26*(26 + 1)));
+    generateRowLetter(numRows);
+    console.log(alphabet1);
     let row1 = "<tr><td></td>";
     for(let z = 1; z<(numSeats+1); z++){
         row1 += "<th>" + z + "</th>";
     }
-    for(let j = 1; j<(numRows+1); j++) {
+    for(let j = 0; j<(numRows); j++) {
 
         row1 += "<tr>";
-        row1 += "<th>" + j + "</th>";
+        row1 += "<th>" + alphabet1[j] + "</th>";
         for(let i = 1; i<(numSeats+1) ; i++) {
-            row1+="<td><input id='" + j + '.' +  i + "' type='checkbox'></td>";
+            row1+="<td><input id='" + alphabet1[j] + '.' +  i + "' type='checkbox'></td>";
         }
-        row1 += "<th>" + j + "</th>";
+        row1 += "<th>" + alphabet1[j] + "</th>";
         row1 += "</tr>";
 
     }
