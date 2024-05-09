4c9aecf64a7ba81a363fa48db2bdfdcd08e496ef
kailashrao <kailash7rao@gmail.com>
2024-05-09 15:56:01 -0700

    Update Count OnKeyUp (instead of OnChange)

--- Character Limit Bar/app.js
+++ Character Limit Bar/app.js
@@ -1,8 +1,9 @@
 function getCharLeft(limit, id) {
     const input = document.getElementById("text-box" + id).value;
+    console.log(input);
     const currentSize = input.length;
     let charLeft = (currentSize < limit) ? limit - currentSize : 0;
-    console.log(charLeft);
+    // console.log(charLeft);
     document.getElementById("message" + id).innerHTML = "Characters Left: " + charLeft;
 }
 
